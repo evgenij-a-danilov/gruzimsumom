f40720d1af64d9e8a95bbcd069c9fc126ae7114f
Mikhail Anufriev <mikhailanufriev12@gmail.com>
2020-10-18 14:08:25 +0600

    OrderController revision

---
+++ billing/src/main/java/ru/gruzimsumom/erp/ordertracker/billing/controller/OrderController.java
@@ -0,0 +1,47 @@
+package ru.gruzimsumom.erp.ordertracker.billing.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.gruzimsumom.erp.ordertracker.billing.Order;
+import ru.gruzimsumom.erp.ordertracker.billing.OrderDAO;
+
+import java.util.List;
+
+//import com.journaldev.jackson.model.Address;
+//import com.journaldev.jackson.model.Employee;
+//import com.example2.demo2.LocationDAO;
+//import com.example2.demo2.LocationTransactionException;
+//import com.example2.demo2.Location;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+
+
+    @Autowired
+    private OrderDAO orderDAO;
+
+    @GetMapping()
+    public List<Order> getOrder() {
+        List<Order> orderList = orderDAO.getOrders();
+
+        return orderList;
+    }
+
+    @GetMapping("/{id}")
+    public String getLastOrderId(@PathVariable("id") String id) {
+        String orderList = orderDAO.getOrderByID(id);
+
+        return orderList;
+    }
+
+//    @RequestMapping("/")
+//    public String index() {
+//        return "Greetings from Spring Boot!";
+//    }
+
+}
